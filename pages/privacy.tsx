eb9f1cdeb9f07a5a7b5d0f287c612f677c0e8cce
wolf760 <sdarko581@gmail.com>
2025-06-11 14:01:39 +0200

    Add index, privacy and terms pages

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -2,7 +2,8 @@ export default function Privacy() {
   return (
     <div>
       <h1>Privacy Policy</h1>
-      <p>This is where your app’s privacy policy will appear.</p>
+      <p>This is our privacy policy.</p>
     </div>
   );
 }
+
--- pages/terms.tsx
+++ pages/terms.tsx
@@ -2,7 +2,8 @@ export default function Terms() {
   return (
     <div>
       <h1>Terms & Conditions</h1>
-      <p>These are the terms and conditions for using Moodify.</p>
+      <p>These are the terms and conditions.</p>
     </div>
   );
 }
+
